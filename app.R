37468de677606cefb4f263ae12aca0889b89fef8
Justin Hoffman <jmh120804@gmail.com>
2026-04-30 20:19:06 -0500

    Chloropleth coloring

--- app.R
+++ app.R
@@ -5,6 +5,11 @@ library(sf)
 
 master <- readRDS("data/master_metros.rds")
 
+pal <- colorNumeric(
+  palette = "YlOrRd",
+  domain  = master$afford_score
+)
+
 ui <- fluidPage(
   leafletOutput("map", height = "600px")
 )
@@ -13,7 +18,13 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet(master) %>%
       addTiles() %>%
-      addPolygons()
+      addPolygons(
+        fillColor   = ~pal(afford_score),
+        fillOpacity = 0.7,
+        color       = "#444444",
+        weight      = 1,
+        smoothFactor = 0.5
+      )
   })
 }
 
